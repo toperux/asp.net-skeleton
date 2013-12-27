9f334f1951e2c5f9a7d9ffb3cee6fc173eab15c2
Allan Espinosa <allan.espinosa@outlook.com>
2013-12-27 20:42:20 +0900

    Create sample test

---
+++ Allan.UnitTests/SampleTests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace Allan.UnitTests
+{
+    public class SampleTests
+    {
+        [Fact]
+        public void SomeTest()
+        {
+            Assert.True(true);
+        }
+    }
+}
