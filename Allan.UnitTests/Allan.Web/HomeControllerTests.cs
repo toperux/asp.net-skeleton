329be65ba5e0ef454608dc7736b63b895b9f608a
Allan Espinosa <allan.espinosa@outlook.com>
2014-02-09 01:46:35 +0900

    Add a skeleton controller

---
+++ Allan.UnitTests/Allan.Web/HomeControllerTests.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+
+using Allan.Web.Controllers;
+
+namespace Allan.UnitTests.Allan.Web
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void Index_ByDefault_ReturnsHelloWorld()
+        {
+            HomeController home = new HomeController();
+
+            var result = home.Index();
+
+            Assert.Equal("Hello World", result);
+        }
+    }
+}
---
+++ Allan.Web/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Allan.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Hello World";
+        }
+    }
+}
