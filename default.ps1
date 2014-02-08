4354782285160f1780218861b3dfdd4e6549c780
Allan Espinosa <allan.espinosa@outlook.com>
2014-02-08 16:47:17 +0900

    Replace msbuild test task with psake

---
+++ default.ps1
@@ -0,0 +1,19 @@
+properties {
+  $cwd = Resolve-Path .
+  $xunitRunner = "$cwd\packages\xunit.runners.1.9.2\tools\xunit.console.clr4.exe"
+}
+
+Task -name Build -action {
+  Exec {
+    msbuild Allan.EntityFramework.sln
+  }
+};
+
+Task -name UnitTest -depend Build -action {
+  Mkdir TestResults
+  Exec {
+    & $xunitRunner ".\Allan.UnitTests\bin\Debug\Allan.UnitTests.dll" /nunit TestResults\Allan.UnitTests.xml
+  }
+}
+
+Task -name Default -depends Build
